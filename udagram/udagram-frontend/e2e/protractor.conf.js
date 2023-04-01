242d116b853c545742385589a6c501ceb901b681
emankhalifa101 <emankhalifa101@gmail.com>
2023-04-01 10:58:32 +0200

    update circleci configs

--- udagram/udagram-frontend/e2e/protractor.conf.js
+++ udagram/udagram-frontend/e2e/protractor.conf.js
@@ -12,7 +12,7 @@ exports.config = {
     'browserName': 'chrome'
   },
   directConnect: true,
-  baseUrl: 'http://localhost:4200/',
+  baseUrl: 'http://udagram-api-dev.eba-yaici2df.us-east-1.elasticbeanstalk.com:8080/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
